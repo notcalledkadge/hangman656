fdc2e2c23a476111b704bba07ffd76d2b4c7e4f7
Kaj Smith <kajsok@outlook.com>
2024-05-31 21:29:08 +0100

    Created repository, and inserted .py file containing variables

---
+++ Milestone_Repository/Milestone_2.py
@@ -0,0 +1,26 @@
+import random 
+
+def word_list():
+   fruits = ["banana", "strawberry", "orange", "apples", "kiwi"]
+   print(fruits)
+# word_list()
+
+def word():
+   fruits = ["banana", "strawberry", "orange", "apples", "kiwi"]
+   random_fruit = random.choice(fruits)
+   print(random_fruit)
+# word()
+
+def guess():
+   letter = input("Please only input a single letter:").lower()
+   #print(len(letter))
+   #if len(letter) == 1 and isinstance(letter, str):
+   if len(letter) ==1 and letter.isalpha():
+    # return print("Ooh, that's the right length")
+    return print("Good guess!")
+    # elif len(letter) > 1 or type(letter) == int:
+   else:
+    return print("Oops! That is not a valid input.")
+# guess()
+   
+   
