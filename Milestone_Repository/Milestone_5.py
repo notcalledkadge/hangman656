cbd1f7acc3ffa9afa428223065e311bc77f6d4bc
Kaj Smith <kajsok@outlook.com>
2024-06-04 14:04:40 +0100

    "Completed Milestone_5. Updated README. Tidied."

--- Milestone_Repository/Milestone_5.py
+++ Milestone_Repository/Milestone_5.py
@@ -1,14 +1,12 @@
 
-### Create the Class.
-
 import random
-word_list = ["one","two","three","four","five"]
+
+word_list = ["black","dumbledore","hagrid","lupin","malfoy","mcgonagall","potter","snape"]
 word = random.choice(word_list)
 word_guessed = ['_' for _ in word]
 guess_list = [""]
 num_letters = len(set(word_guessed))
-guess = input("Enter a letter: ").lower() 
-
+num_lives = 5
 
 class Hangman:
    def __init__(self):
@@ -16,12 +14,8 @@ class Hangman:
        self.word_guessed = ['_' for _ in self.word]
        self.num_letters = len(set(self.word))
        self.word_list = word_list
-       self.num_lives = 3
+       self.num_lives = num_lives
        self.guess_list = guess_list
-  
-### Create methods for running the checks.
-### Define what happens when the letter is in the chosen word.
-### Define what happens when the letter is NOT in the chosen word.
 
    def check_guess(self, guess):
         global word_guessed, num_letters
@@ -35,21 +29,42 @@ class Hangman:
         else:
             print("Sorry,",guess,"is not in the word")
             self.num_lives -= 1
-            print("You have",self.num_lives,"lives left.")
+            if self.num_lives >1:
+             print("You have",self.num_lives,"lives left.")
+            if self.num_lives ==1:
+             print("You have",self.num_lives,"life left.")
         print(word_guessed)
-        print(self.num_letters)
+
 
    def ask_for_input(self):
+        guess = input("Enter a letter: ").lower() 
         while True:
             if len(guess) != 1 or not guess.isalpha():
              return print("Invalid input. Please enter a single alphabetical character.")
+             
             elif guess in self.guess_list:
              return print("You already tried that letter!")
+             
             else:
                 self.check_guess(guess)
                 self.guess_list.append(guess)
                 break
 
+### Code the logic of the game.
 
-# hangman_game = Hangman()
-# hangman_game.ask_for_input()
+def play_game():
+    print(word_guessed)
+    game = Hangman()
+    while True:
+         if game.num_lives == 0:
+          print ("You lost!")
+          break
+         elif game.num_letters > 0:
+          game.ask_for_input() 
+         else:
+          print("Congratulations! You win! The word was:",word)
+          break
+
+play_game()
+                   
+       
